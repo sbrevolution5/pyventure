98c74322478d82ead34484c5b8914420fcda9f42
sbrevolution5 <Sbrevolution5@aim.com>
2021-03-08 10:42:31 -0500

    added quit function and made whatdo function to be reusable

--- pyventure.py
+++ pyventure.py
@@ -1,4 +1,4 @@
-# a very brief text adventure, with easter eggs for my wife, like milking the monster.
+# a space based text adventure
 from sys import exit
 startofgame = True
 inventory = []
@@ -9,40 +9,7 @@ def charmaker():
 def cockpit(charName, firsttime):
     print(f"You sit in your cockpit, aboard the starship Everess, as it comes out of hyperspace.  You take a second to remember your bearings.  You are {charName}, esteemed captain of the Everess, not that anyone really reports to you, since you are a lone mercenary, sailing the black winds of space, pinpricks of starlight on the horizon.  In the cockpit you see your laser pistol, water bottle, snack stash, Oxygen rebreather, the picture of Becky (your long ago love that you bought the Everess to impress before she.... well... better to focus on you for now....) and emergency flashlight. \n Red lights begin to blink across the console of your ship.  You see your engines begin to fail, oxygen levels dropping, and just before the screen goes blank, you see the characters 'INTRU' crawl across the screen. ")
     print(f"now is the time for action.")
-    print(f"What do you do?")
-    while(True):
-        choice= input("> ")
-        if "leave" in choice:
-            print("you go into the hallway")
-            hallway(charName)
-        elif "take" in choice:
-            if "laser" in choice or "pistol" in choice:
-                print("You pick up the laser pistol")
-                inventory.append("Laser Pistol")
-            if "oxygen" in choice:
-                print("You pick up the Oxygen rebreather")
-                inventory.append("Oxygen Rebreather")
-            if "snack" in choice:
-                print("You pick up the Snacks")
-                inventory.append("Snack")
-
-            if "picture" in choice:
-                print("You pick up the picture of Becky")
-                inventory.append("Becky's Picture")
-
-            if "flashlight" in choice:
-                print("You pick up the emergency flashlight, only to find that it isn't working, batteries must be dead...  You remember that there were some battery powered toys in the cargo bay, but without the computer it will be tough to sort through the inventory.")
-                inventory.append("dead flashlight")
-
-            if "water" in choice:
-                print("You pick up the water bottle")
-                inventory.append("water bottle")
-        elif "HELP" in choice:
-            help()
-        elif "INV" in choice:
-            get_inventory()
-        else:
-            print("Please input a valid action, type HELP for some examples")
+    whatdo(charname, "cockpit")
 
 def hallway(charName):
     print(f"As {charName}, who is you just in case you forgot, enters the hallway, you remember the general layout.  You can go to the following locations: \n\t(1) Cockpit\n\t(2) Mess Hall\n\t(3) Armory\n\t(4) Airlock \n\t(5) Medbay\n\t(6) Cargo Hold\n\t(7) Escape Hatch\n\t(8) Janitor Closet")
@@ -97,6 +64,46 @@ def escapeHatch(charName):
     dead("There isn't air in the escape hatch", charName)
 def janitorCloset(charName):
     print("You arrive in the Janitor's closet")
+
+
+# Utility functions ____________
+def whatdo(charname, roomname):
+    print(f"What do you do?")
+    while(True):
+        choice= input("> ")
+        if "leave" in choice:
+            print("you go into the hallway")
+            hallway(charName)
+        elif "EXIT" in choice or "QUIT" in choice:
+            quitter()
+        elif "take" in choice:
+            if "laser" in choice or "pistol" in choice:
+                print("You pick up the laser pistol")
+                inventory.append("Laser Pistol")
+            if "oxygen" in choice:
+                print("You pick up the Oxygen rebreather")
+                inventory.append("Oxygen Rebreather")
+            if "snack" in choice:
+                print("You pick up the Snacks")
+                inventory.append("Snack")
+
+            if "picture" in choice:
+                print("You pick up the picture of Becky")
+                inventory.append("Becky's Picture")
+
+            if "flashlight" in choice:
+                print("You pick up the emergency flashlight, only to find that it isn't working, batteries must be dead...  You remember that there were some battery powered toys in the cargo bay, but without the computer it will be tough to sort through the inventory.")
+                inventory.append("dead flashlight")
+
+            if "water" in choice:
+                print("You pick up the water bottle")
+                inventory.append("water bottle")
+        elif "HELP" in choice:
+            help()
+        elif "INV" in choice:
+            get_inventory()
+        else:
+            print("Please input a valid action, type HELP for some examples")
 def dead(why,charName):
     print(why, f"  Nice going, but {charName}'s dead!")
     print("GAME OVER")
@@ -106,8 +113,17 @@ def get_inventory():
     print(inventory)
 
 def help():
-    print("If you are given a list of numbers, type in a number to make a choice.  Otherwise, use simple commands like take flashlight, leave cockpit, open door etc. use capital letters to access 'Out of character' functions like HELP or INVentory")
+    print("If you are given a list of numbers, type in a number to make a choice.  Otherwise, use simple verb-noun commands like 'take flashlight', 'leave cockpit', 'open door' etc. use capital letters to access 'Out of character' functions like HELP, EXIT or INVentory")
     return
+def quitter():
+    print("are you sure you want to quit?  y/n")
+    while(True):
+        quitting = input("> ")
+        if "y" in quitting:
+            print("goodbye!")
+            exit()
+        elif "n" in quitting:
+            return
 def game():
     print("Game start!")
     charname = charmaker()
